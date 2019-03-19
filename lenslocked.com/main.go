d67950d1d4c3ec1ef4461a2564916c6c663f7d47
Anton Mendelson <1choice@mail.ru>
2019-03-19 17:56:09 +0300

    initial

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -20,20 +20,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
-
 func main() {
-	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
-	//mux := &http.ServeMux{}
-	//mux.HandleFunc("/", handlerFunc)
-	// http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3010", router)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3010", r)
 }
