85d08e4de15eddaf0007cf019bc4d579be443833
Anton Mendelson <1choice@mail.ru>
2019-03-19 17:53:15 +0300

    httprouter commit

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -11,11 +13,27 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to main</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "<h1>Welcome to contact</h1>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>PAGE NOT FOUND")
 	}
 
 }
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3010", nil)
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+	//mux := &http.ServeMux{}
+	//mux.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3010", router)
 }
